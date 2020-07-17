008f6803385b484e5b4d35978ecb0e500bf2b954
HosseinSaneifar <saneifar.h@gmail.com>
2020-07-16 16:03:37 -0800

    First Edition To show

--- TestSolution/Client/Pages/IndexPage/IndexViewModel.cs
+++ TestSolution/Client/Pages/IndexPage/IndexViewModel.cs
@@ -15,7 +15,14 @@ namespace TestSolution.Client.Pages.IndexPage
                 new TaskItem(2, "Add My Name To Page", DateTime.Parse("2020-07-16"),
                     "Change Hello World to Have My Introduction "),
                 new TaskItem(3, "Create TaskList & TaskItem Component", DateTime.Parse("2020-07-16"),
-                    "To Show All Tasks That I'v Done I Create it")
+                    "To Show All Tasks That I'v Done I Create it"),
+                new TaskItem(4, "Start Changing Structure To MVVM", DateTime.Parse("2020-07-16"),
+                    "At First Separating View from Viewmodel So We Have Cleaner Code and more Readability"),
+                new TaskItem(5, "Joining First Solution To The Project", DateTime.Parse("2020-07-16"),
+                    "All First Chat Application Feature Added To this Project"),
+                new TaskItem(6, "Start Joining Second One", DateTime.Parse("2020-07-16"),
+                    "After First joining Try I Get Error And I have to go Somewhere So Still Needs To complete "),
+
             };
         }
     }
